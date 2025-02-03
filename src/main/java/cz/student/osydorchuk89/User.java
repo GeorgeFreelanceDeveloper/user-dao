ce666f02f64fc4a982cd1a0e2eaff473f32b7a22
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2025-02-03 20:11:24 +0100

    updated workshop solution

--- src/main/java/cz/student/osydorchuk89/User.java
+++ src/main/java/cz/student/osydorchuk89/User.java
@@ -1,5 +1,7 @@
 package cz.student.osydorchuk89;
 
+import java.util.Objects;
+
 public class User {
     private int id;
     private String userName;
@@ -45,7 +47,20 @@ public class User {
         this.password = password;
     }
 
+    @Override
     public String toString() {
         return "\nId: " + id + "\nUser name: " + userName + "\nEmail: " + email + "\nHashed password: " + password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(userName, user.userName) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, email, password);
     }
 }
--- src/main/java/cz/student/osydorchuk89/UserDao.java
+++ src/main/java/cz/student/osydorchuk89/UserDao.java
@@ -1,5 +1,6 @@
 package cz.student.osydorchuk89;
 
+import cz.common.DbUtil;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
@@ -8,13 +9,18 @@ import java.util.List;
 
 public class UserDao {
 
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String GET_USER_BY_ID_QUERY = "SELECT * FROM USERS WHERE id = ?";
+    private static final String UPDATE_USER_QUERY = "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?";
+    private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = ?";
+    private static final String GET_ALL_USERS_QUERY = "SELECT * FROM users";
+
     public User createUser(String userName, String email, String password) {
 
-        String createUserQuery = "insert into users (username, email, password) values (?, ?, ?)";
-
         try (Connection conn = DbUtil.getConnection()) {
-            PreparedStatement statement =
-                    conn.prepareStatement(createUserQuery, Statement.RETURN_GENERATED_KEYS);
+            final PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, userName);
             statement.setString(2, email);
             statement.setString(3, hashPassword(password));
@@ -27,19 +33,18 @@ public class UserDao {
                 return null;
             }
         } catch (SQLException e) {
+            System.err.println("Problem with adding user in the database: " + e.getMessage());
             e.printStackTrace(System.err);
             return null;
         }
     }
 
-    public User getUserById(int id) {
-
-        String getUserByIdQuery = "select * from users where id = ?";
+    public User getUserById(int userId) {
 
         try (Connection conn = DbUtil.getConnection()) {
             PreparedStatement statement =
-                    conn.prepareStatement(getUserByIdQuery);
-            statement.setString(1, Integer.toString(id));
+                    conn.prepareStatement(GET_USER_BY_ID_QUERY);
+            statement.setInt(1, userId);
             ResultSet result = statement.executeQuery();
             if (result.next()) {
                 return new User(result.getInt("id"), result.getString("username"), result.getString("email"), result.getString("password"));
@@ -48,54 +53,51 @@ public class UserDao {
                 return null;
             }
         } catch (SQLException e) {
+            System.err.println("Problem with retrieving user from the database: " + e.getMessage());
             e.printStackTrace(System.err);
             return null;
         }
     }
 
-    public void updateUser(User user, String userName, String email, String password) {
-        user.setUserName(userName);
-        user.setEmail(email);
-        user.setPassword(password);
-
-        String updateUserQuery = "update users set username = ?, email = ?, password = ? where id = ?";
+    public void updateUser(int id, String userName, String email, String password) {
 
         try (Connection conn = DbUtil.getConnection()) {
             PreparedStatement statement =
-                    conn.prepareStatement(updateUserQuery);
-            statement.setString(1, user.getUserName());
-            statement.setString(2, user.getEmail());
-            statement.setString(3, hashPassword(user.getPassword()));
-            statement.setString(4, Integer.toString(user.getId()));
+                    conn.prepareStatement(UPDATE_USER_QUERY);
+            statement.setString(1, userName);
+            statement.setString(2, email);
+            statement.setString(3, hashPassword(password));
+            statement.setString(4, Integer.toString(id));
             statement.executeUpdate();
-            System.out.println("User " + user.getUserName() + " updated successfully");
+            System.out.println("User " + userName + " updated successfully");
         } catch (SQLException e) {
+            System.err.println("Problem with updating user in the database: " + e.getMessage());
             e.printStackTrace(System.err);
         }
     }
 
-    public void deleteUserById(int id) {
-        User user = getUserById(id);
-
-        String deleteUserByIdQuery = "delete from users where id = ?";
+    public void deleteUserById(int userId) {
 
         try (Connection conn = DbUtil.getConnection()) {
             PreparedStatement statement =
-                    conn.prepareStatement(deleteUserByIdQuery);
-            statement.setString(1, Integer.toString(user.getId()));
-            statement.executeUpdate();
-            System.out.println("User " + user.getUserName() + " deleted successfully");
+                    conn.prepareStatement(DELETE_USER_QUERY);
+            statement.setInt(1, userId);
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.printf("User with id %s deleted successfully.%n", userId);
+            } else {
+                System.out.printf("User delete with id %s failed. No rows affected.%n", userId);
+            }
         } catch (SQLException e) {
+            System.err.println("Problem with deleting user from the database: " + e.getMessage());
             e.printStackTrace(System.err);
         }
     }
 
     public List<User> getAllUsers() {
-        String getAllUsersQuery = "select * from users";
-
         try (Connection conn = DbUtil.getConnection()) {
             PreparedStatement statement =
-                    conn.prepareStatement(getAllUsersQuery);
+                    conn.prepareStatement(GET_ALL_USERS_QUERY);
             ResultSet result = statement.executeQuery();
             List<User> users = new ArrayList<>();
             while (result.next()) {
