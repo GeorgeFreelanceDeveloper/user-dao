1b464ffaa57116c33a6d4f8ed75131f7c5033b38
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2025-02-18 08:29:22 +0100

    updated workshop solution-3

--- src/main/java/cz/student/osydorchuk89/App.java
+++ src/main/java/cz/student/osydorchuk89/App.java
@@ -10,6 +10,9 @@ public class App {
         User user1  = userDao.createUser("user1", "user1@gmail.com", "password1");
         User user2 = userDao.createUser("user2", "user2@gmail.com", "password2");
         User user3 = userDao.createUser("user3", "user3@gmail.com", "password3");
+        System.out.println("\nGetting user with id " + user3.getId());
+        User user3Copy = userDao.getUserById(user3.getId());
+        System.out.println(user3Copy);
         System.out.println("\nUpdating user with id " + user1.getId());
         userDao.updateUser(user1.getId(), "user1_upd", "user1@gmail.com_upd", "password1_upd");
         System.out.println("\nDeleting user with id " + user3.getId());
